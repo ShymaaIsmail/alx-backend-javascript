1bfb26893b698082055cbc3c30baf14c4f829d36
shymaaismail <shymaa.m.ismail@gmail.com>
2024-06-26 09:27:08 +0300

    tas 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  const first_promise = Promise;
+  if (success) {
+    return first_promise.resolve({
+      status: 200,
+      body: 'Success',
+    });
+  }
+  return first_promise.reject('The fake API is not working currently');
+}
