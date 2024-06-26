3ae336a10b36b15d313dad9fbb0243d7b8524ef9
shymaaismail <shymaa.m.ismail@gmail.com>
2024-06-26 11:34:20 +0300

    task 7

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.any([chinaDownload, USDownload])
-    .then((res) => res);
+  return Promise.race([chinaDownload, USDownload]);
 }
