7e7f480abe2dfefca3c07d97f3296c9523715b4e
shymaaismail <shymaa.m.ismail@gmail.com>
2024-06-26 11:11:57 +0300

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,14 +3,5 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      const finalResult = [];
-      results.forEach((result) => {
-        finalResult.push({
-          status: result.status,
-          value: result.value !== undefined ? result.value : result.reason,
-        });
-        return finalResult;
-      });
-    });
+    .then((results) => results);
 }
