eb27c28cf9222a0c8a7941588b073d2095223166
shymaaismail <shymaa.m.ismail@gmail.com>
2024-06-30 09:04:45 +0300

    task 100

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -8,11 +8,20 @@ export default class Car {
   }
 
   [cloneSymbol]() {
-    const Constructor = this.constructor;
+    const Constructor = this.constructor[Symbol.species];
+    if (Constructor === Car) {
+      // Clone called on Car instance, return default Car instance
+      return new Car();
+    }
+    // Clone called on subclass instance, return instance of that subclass
     return new Constructor(this._brand, this._motor, this._color);
   }
 
   cloneCar() {
     return this[cloneSymbol]();
+  }
+
+  static get [Symbol.species]() {
+    return this;
   }
 }
