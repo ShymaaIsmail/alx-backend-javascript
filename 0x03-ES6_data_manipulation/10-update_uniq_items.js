aa33f4d9ca25bb7b0f53619766679d64aea7d6ec
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-01 12:03:19 +0300

    task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+const updateUniqueItems = (map) => {
+  if (map instanceof Map) {
+    map.forEach((value, key) => {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    });
+  } else throw new Error('Cannot process');
+  return map;
+};
+export default updateUniqueItems;
