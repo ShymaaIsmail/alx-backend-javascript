53807b335ab0ee1d36f9bb06609ba484ff183f07
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-02 10:19:25 +0300

    100 fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,10 @@
 const weakMap = new WeakMap();
 const queryAPI = (endpoint) => {
   if (typeof (endpoint) === 'object') {
-    let queries = weakMap.get(endpoint);
+    let queries = weakMap.get(endpoint) || 0;
+    queries += 1;
     if (queries >= 5) throw new Error('Endpoint load is high');
     else {
-      queries = queries === undefined ? 1 : queries + 1;
       weakMap.set(endpoint, queries);
     }
   }
