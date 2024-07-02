91547450743dc30d3ed0710e31d6cfdd6dc40f63
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-02 09:05:16 +0300

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 const cleanSet = (set, startString) => {
   let cleanText = '';
-  if (typeof startString === 'string' && startString.length > 0) {
+  if ((set instanceof Set) && typeof startString === 'string' && startString.length > 0) {
     cleanText = [...set].reduce((accumulator, currentValue) => {
       if (typeof currentValue === 'string' && currentValue.startsWith(startString)) {
         const cleanedValue = currentValue.replace(startString, '');
