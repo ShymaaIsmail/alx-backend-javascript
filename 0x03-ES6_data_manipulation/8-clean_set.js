07746bafc13bffe48698cd3f62c46ee0c1787f6d
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-02 08:56:56 +0300

    8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,13 @@
 const cleanSet = (set, startString) => {
   let cleanText = '';
-  if (typeof set === 'object' && typeof startString === 'string' && startString.length > 0) {
+  if (typeof startString === 'string' && startString.length > 0) {
     cleanText = [...set].reduce((accumulator, currentValue) => {
-      if (startString.length > 0) {
-        if (currentValue.startsWith(startString)) {
-          const cleanedValue = currentValue.replace(startString, '');
-          if (accumulator.length === 0) {
-            return cleanedValue;
-          }
-          return `${accumulator}-${cleanedValue}`;
+      if (currentValue.startsWith(startString)) {
+        const cleanedValue = currentValue.replace(startString, '');
+        if (accumulator.length === 0) {
+          return cleanedValue;
         }
+        return `${accumulator}-${cleanedValue}`;
       }
       return accumulator;
     }, '');
