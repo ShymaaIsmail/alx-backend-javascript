1ed0c63ec5b840d481f6ab7c04b90b0d75dc990d
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-08-24 01:14:18 +0300

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,21 +1,9 @@
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api'); // Import the Express app
+
 const port = 7865;
 
 describe('Index page', () => {
-  let server;
-
-  before((done) => {
-    // Start the server before tests
-    server = app.listen(port, done);
-  });
-
-  after((done) => {
-    // Close the server after tests
-    server.close(done);
-  });
-
   it('should return status code 200', (done) => {
     request(`http://localhost:${port}`, (err, res) => {
       if (err) return done(err);
