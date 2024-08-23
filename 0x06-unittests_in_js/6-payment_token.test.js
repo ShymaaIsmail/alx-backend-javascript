f3bb1b9542e638d8c4ceb02a116435ac51f7b9bb
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-08-24 00:52:32 +0300

    Update 6-payment_token.test.js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,66 +1,18 @@
-// 6-payment_token.test.js
+const mocha = require('mocha');
 const { expect } = require('chai');
+const sinon = require('sinon');
+
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-    it('should resolve with a successful response when success is true', (done) => {
-        getPaymentTokenFromAPI(true)
-            .then((response) => {
-                try {
-                    expect(response).to.deep.equal({ data: 'Successful response from the API' });
-                    done(); // Indicate that the test is complete
-                } catch (error) {
-                    done(error); // Pass any assertion errors to done
-                }
-            })
-            .catch(done); // Handle any unexpected errors
-    });
-
-    it('should not resolve when success is false', (done) => {
-        getPaymentTokenFromAPI(false)
-            .then((response) => {
-                try {
-                    expect(response).to.be.undefined; // Assuming no response or resolve
-                    done(); // Indicate that the test is complete
-                } catch (error) {
-                    done(error); // Pass any assertion errors to done
-                }
-            })
-            .catch(done); // Handle any unexpected errors
-    });
-
-    it('should reject the promise if success is not a boolean', (done) => {
-        // Test with an invalid success value
-        getPaymentTokenFromAPI('not-a-boolean')
-            .then(() => {
-                done(new Error('Expected promise to be rejected'));
-            })
-            .catch((error) => {
-                try {
-                    expect(error).to.be.instanceOf(TypeError); // Assuming TypeError for invalid input
-                    done();
-                } catch (err) {
-                    done(err);
-                }
-            });
-    });
-
-    it('should handle if the function does not return a promise', (done) => {
-        // Temporarily stub the function to return a non-promise value
-        const originalFunction = getPaymentTokenFromAPI;
-        getPaymentTokenFromAPI = () => 'Not a promise';
-
-        try {
-            getPaymentTokenFromAPI(true)
-                .then(() => {
-                    done(new Error('Expected promise to be rejected'));
-                })
-                .catch(() => {
-                    done(); // This should be called if the function is incorrect
-                });
-        } finally {
-            // Restore the original function
-            getPaymentTokenFromAPI = originalFunction;
-        }
-    });
+  it('should resolve promise if success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.eql({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
 });
