632bb6cd714f0a3626ce64957b646dcf3cd7e57c
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-08-24 00:35:54 +0300

    Create 6-payment_token.js

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,12 @@
+// 6-payment_token.js
+function getPaymentTokenFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if (success) {
+            resolve({ data: 'Successful response from the API' });
+        } else {
+            // Do nothing
+        }
+    });
+}
+
+module.exports = getPaymentTokenFromAPI;
