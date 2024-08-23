b7c1349b88a8aadf4333bfe78b504c149f9885ae
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-08-23 23:52:24 +0300

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -17,4 +17,9 @@ describe('calculateNumber', () => {
     it('should return 6 when inputs are 1.5 and 3.7', () => {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
+
+    it('should return 6 when inputs are 1.5 and 3', () => {
+        assert.strictEqual(calculateNumber(1.5, 3), 5);
+    });
 });
+
